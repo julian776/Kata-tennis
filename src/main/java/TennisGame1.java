27e075df1d257b8ec87a697270cbcbc5db4c917b
julian776 <juliancamiloalvarez77@gmail.com>
2021-12-17 08:42:33 -0500

    Se arreglo unos detalles, sigue siendo irrelevante arreglar las recomendaciones de complejidad ya que solo dificultarian la lectura del codigo

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,11 +1,10 @@
-import java.util.List;
 
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int score1 = 0;
+    private int score2 = 0;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -14,16 +13,16 @@ public class TennisGame1 implements TennisGame {
 
     public void wonPoint(String playerName) {
         if (playerName.equals(player1Name))
-            m_score1 += 1;
+            score1 += 1;
         else
-            m_score2 += 1;
+            score2 += 1;
     }
 
     public String getScore() {
         String score;
-        if (m_score1 == m_score2) {
+        if (score1 == score2) {
             score = validateEquals();
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
+        } else if (score1 >= 4 || score2 >= 4) {
             score = validateEndPoint();
         } else {
             score = validatePlayingPoint();
@@ -33,11 +32,11 @@ public class TennisGame1 implements TennisGame {
 
     private String validatePlayingPoint() {
         String[] points = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-        return points[m_score1] + "-" + points[m_score2];
+        return points[score1] + "-" + points[score2];
     }
 
     private String validateEndPoint() {
-        int minusResult = m_score1 - m_score2;
+        int minusResult = score1 - score2;
         if (minusResult == 1) return "Advantage player1";
         if (minusResult == -1) return "Advantage player2";
         return minusResult >= 2 ? "Win for player1" : "Win for player2";
@@ -46,6 +45,6 @@ public class TennisGame1 implements TennisGame {
 
     private String validateEquals() {
         String[] points = new String[]{"Love-All", "Fifteen-All", "Thirty-All", "Deuce"};
-        return m_score1 >= 3 ? "Deuce" : points[m_score1];
+        return score1 >= 3 ? "Deuce" : points[score1];
     }
 }
